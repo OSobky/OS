e2e6d0389d09f9d792ad4097d385dcbfe86f5b28
OSobky <Omar_sobky@hotmail.com>
2018-04-18 18:12:16 +0200

    player and some op

--- OS/src/Operator.java
+++ OS/src/Operator.java
@@ -1,4 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Queue;
 
 public class Operator {
+	static Wheel wheel ;
+	Queue<Player> Player ;
+	
+	public static void main(String[] args) {
+		 String fileName = "temp.txt";
 
+	        // This will reference one line at a time
+	        String line = null;
+
+	        try {
+	            // FileReader reads text files in the default encoding.
+	            FileReader fileReader = 
+	                new FileReader(fileName);
+
+	            // Always wrap FileReader in BufferedReader.
+	            BufferedReader bufferedReader = 
+	                new BufferedReader(fileReader);
+
+	            while((line = bufferedReader.readLine()) != null) {
+	                System.out.println(line);
+	            }   
+
+	            // Always close files.
+	            bufferedReader.close();         
+	        }
+	        catch(Exception ex) {
+	            System.out.println(
+	                "Unable to open file '" + 
+	                fileName + "'");                
+	        }
+	        
+	        
+	        String lines[] = line.split("\\r?\\n");
+	        
+	        wheel = new Wheel() ;
+		
+		
+		
+	}
+	
 }
--- OS/src/Player.java
+++ OS/src/Player.java
@@ -1,4 +1,25 @@
 
-public class Player {
 
+public class Player extends Thread   {
+	 int ID  ;
+	 int waiting_time ; 
+	 boolean on_board = false ;
+	 boolean ride_complete = false ;
+	
+	 
+	 public Player(int ID , int waiting_time){
+		 this.ID = ID ; 
+		 this.waiting_time = waiting_time ;
+		 
+		
+	 }
+	 
+	 public void run(){
+		try {
+			Thread.sleep(waiting_time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
 }
